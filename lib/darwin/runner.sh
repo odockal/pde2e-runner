c1e1cad8bf745218c962b473339a8d29c26c24f0
Tamara Babalova <tbabalov@redhat.com>
2025-02-04 11:21:27 -0500

    remove compose binary after workflow
    Signed-off-by: Tamara Babalova <tbabalov@redhat.com>

--- lib/darwin/runner.sh
+++ lib/darwin/runner.sh
@@ -394,4 +394,10 @@ if (( cleanMachine == 1 )); then
     podman machine reset -f
 fi
 
+composeBinary="/usr/local/bin/docker-compose"
+if [ -f "$composeBinary" ]; then
+    echo "Removing compose binary file"
+    rm "$composeBinary"
+fi 
+
 echo "Script finished..."
