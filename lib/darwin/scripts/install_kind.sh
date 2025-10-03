ed6af40ed368a6ce548d2b669f10e6ba3053846c
Anton Misskii <amisskii@redhat.com>
2025-10-03 13:06:16 +0200

    fix: avoid using gh token when installing binaries not from gh

--- lib/darwin/scripts/install_kind.sh
+++ lib/darwin/scripts/install_kind.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-set -e 
+set -e
 
 echo "Installing kind..."
 KIND_VERSION=$(curl -s https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq -r .tag_name)
-
 # Install kind based on the arch
 if [ $(uname -m) = arm64 ]; then
-    URL="https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-darwin-arm64"
+    curl -Lo ./kind "https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-darwin-arm64"
 elif [ $(uname -m) = x86_64 ]; then
-    URL="https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-darwin-amd64" 
+    curl -Lo ./kind "https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-darwin-amd64"
 else
     echo "Unsupported architecture: $(uname -m)"
     exit 1
-fi
-
-echo "Downloading kind from $URL"
-if [ -n "$GITHUB_TOKEN" ]; then
-    curl -H "Authorization: Bearer $GITHUB_TOKEN" -L -o ./kind "$URL"
-else
-    echo "Warning: GITHUB_TOKEN is not set."
-    curl -L -o ./kind "$URL"
 fi
 
 chmod +x ./kind
--- lib/darwin/scripts/install_kubectl.sh
+++ lib/darwin/scripts/install_kubectl.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-set -e 
+set -e
 
 echo "Installing kubectl..."
 KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-
 # Install kubectl based on the arch
 if [ $(uname -m) = arm64 ]; then
-    URL="https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/darwin/arm64/kubectl"
+    curl -Lo ./kubectl "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/darwin/arm64/kubectl"
 elif [ $(uname -m) = x86_64 ]; then
-    URL="https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/darwin/amd64/kubectl"
-else
-    echo "Unsupported architecture: $(uname -m)"
-    exit 1
-fi
-
-echo "Downloading kubectl from $URL"
-if [ -n "$GITHUB_TOKEN" ]; then
-    curl -H "Authorization: Bearer $GITHUB_TOKEN" -L -o ./kubectl "$URL"
-else
-    echo "Warning: GITHUB_TOKEN is not set."
-    curl -L -o ./kubectl "$URL"
+    curl -Lo ./kubectl "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/darwin/amd64/kubectl"
 fi
 
 chmod +x ./kubectl
