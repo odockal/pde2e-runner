e194e3f619891d9fbb33f04bc5f796216351f19d
Anton Misskii <amisskii@redhat.com>
2025-10-03 11:20:23 +0200

    use gh token when installing cli tools when its set

--- lib/darwin/scripts/install_kind.sh
+++ lib/darwin/scripts/install_kind.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+set -e 
+
 echo "Installing kind..."
-
 KIND_VERSION=$(curl -s https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq -r .tag_name)
-
 
 # Install kind based on the arch
 if [ $(uname -m) = arm64 ]; then
-    curl -Lo ./kind https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-darwin-arm64
+    URL="https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-darwin-arm64"
 elif [ $(uname -m) = x86_64 ]; then
-    curl -Lo ./kind https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-darwin-amd64
+    URL="https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-darwin-amd64" 
+else
+    echo "Unsupported architecture: $(uname -m)"
+    exit 1
 fi
 
+echo "Downloading kind from $URL"
+if [ -n "$GITHUB_TOKEN" ]; then
+    curl -H "Authorization: Bearer $GITHUB_TOKEN" -L -o ./kind "$URL"
+else
+    echo "Warning: GITHUB_TOKEN is not set."
+    curl -L -o ./kind "$URL"
+fi
 
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
--- lib/darwin/scripts/install_kubectl.sh
+++ lib/darwin/scripts/install_kubectl.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-echo "Installing kubectl..."
+set -e 
 
+echo "Installing kubectl..."
 KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
 
 # Install kubectl based on the arch
 if [ $(uname -m) = arm64 ]; then
-    curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/darwin/arm64/kubectl"
+    URL="https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/darwin/arm64/kubectl"
 elif [ $(uname -m) = x86_64 ]; then
-    curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/darwin/amd64/kubectl"
+    URL="https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/darwin/amd64/kubectl"
+else
+    echo "Unsupported architecture: $(uname -m)"
+    exit 1
+fi
+
+echo "Downloading kubectl from $URL"
+if [ -n "$GITHUB_TOKEN" ]; then
+    curl -H "Authorization: Bearer $GITHUB_TOKEN" -L -o ./kubectl "$URL"
+else
+    echo "Warning: GITHUB_TOKEN is not set."
+    curl -L -o ./kubectl "$URL"
 fi
 
 chmod +x ./kubectl
